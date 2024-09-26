a688d829816e3f7e45e50fa21f5e12ad5c7dd84c
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-09-26 12:52:21 +0500

    new changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,5 @@
-export const URL = 'https://e-commerce-app-alpha-livid.vercel.app';
+export const URL = 'http://localhost:4000';
+
+// https://e-commerce-app-alpha-livid.vercel.app
+
+// http://localhost:4000
